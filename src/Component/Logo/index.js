65b1cffbcdc0dee7657753baf4fffec45147ee22
Wellington Leardini Ramos <73858173+rwellingtonr@users.noreply.github.com>
2021-07-29 23:02:23 -0300

    styled

--- src/Component/Logo/index.js
+++ src/Component/Logo/index.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Tilt from "react-parallax-tilt"
 import "./Logo.css"
-import brain from "./brain.png"
+import alphabet from "./alphabet.png"
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
       <Tilt className="Tilt br2 shadow-2" style={{ height: 100, width: 100 }}>
         <div className="pa1">
-          <img src={brain} alt="logo" />
+          <img src={alphabet} alt="logo" />
         </div>
       </Tilt>
     </div>
