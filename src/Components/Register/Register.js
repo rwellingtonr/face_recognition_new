e1585af982eedeba889298ffde5c6cec4eb12941
rwellingtonr <wellington-158@hotmail.com>
2021-08-06 23:55:54 -0300

    splited project and started to create hooks

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,32 +1,30 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
+import DefaultForm from "../Forms/defaultForm"
+import NameForm from "../Forms/nameForm"
 
-class Register extends Component {
-  constructor(props) {
-    super(props)
-    this.status = {
-      name: "",
-      email: "",
-      password: "",
-    }
-  }
+const Register = (props) => {
+  //Inital State
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   //   change name
-  onNameChange = (event) => {
-    this.setState({ name: event.target.value })
+  const onNameChange = (event) => {
+    console.log(event.target.value)
+    setName(event.target.value)
   }
   // change email
-  onEmailChange = (event) => {
+  const onEmailChange = (event) => {
     this.setState({ email: event.target.value })
   }
   // change password
-  onPasswordChange = (event) => {
+  const onPasswordChange = (event) => {
     this.setState({ password: event.target.value })
   }
   // Submit the registration
-  onClickRegister = async () => {
+  const onClickRegister = async () => {
     try {
-      const { loadUser, onRouteChange } = this.props
-      const { name, email, password } = this.state
+      const { loadUser, onRouteChange } = props
       //write in the Database
       const response = await fetch(
         "https://gentle-caverns-57673.herokuapp.com/register",
@@ -52,67 +50,30 @@ class Register extends Component {
     }
   }
 
-  render() {
-    return (
-      <article className="br3 ba b--black-10 mv4 w-80-ns w-50-m w-25-l mw6 center shadow-5">
-        <div
-          className="pa4"
-          action="sign-up_submit"
-          method="get"
-          acceptCharset="utf-8"
-        >
+  return (
+    <article className="br3 ba b--black-10 mv4 w-80-ns w-50-m w-25-l mw6 center shadow-5">
+      <main className="pa4 black-80">
+        <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f2 fw6 ph0 mh0 center">Register</legend>
-            <div className="mt3">
-              <label className="db fw4 lh-copy f5" htmlFor="Name">
-                Name
-              </label>
-              <input
-                className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
-                type="text"
-                name="Name"
-                id="Name"
-                onChange={this.onNameChange}
-              />
-            </div>
-            <div className="mt3">
-              <label className="db fw4 lh-copy f5" htmlFor="email-address">
-                Email address
-              </label>
-              <input
-                className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
-                type="email"
-                required
-                name="email-address"
-                id="email-address"
-                onChange={this.onEmailChange}
-              />
-            </div>
-            <div className="mt3">
-              <label className="db fw4 lh-copy f5" htmlFor="password">
-                Password
-              </label>
-              <input
-                className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
-                type="password"
-                name="password"
-                id="password"
-                onChange={this.onPasswordChange}
-              />
-            </div>
+            <legend className="f2 fw6 ph0 center mh0">Register</legend>
+            <NameForm onNameChange={onNameChange} />
+            {/* Email and Password */}
+            <DefaultForm
+              onEmailChange={onEmailChange}
+              onPasswordChange={onPasswordChange}
+            />
           </fieldset>
-          <div className="mt3">
+          <div className="">
             <input
-              onClick={this.onClickRegister}
-              className="b ph3 pv2 input-reset ba b--black b--black bg-transparent grow pointer f6 dib"
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Register"
             />
           </div>
         </div>
-      </article>
-    )
-  }
+      </main>
+    </article>
+  )
 }
 
 export default Register
