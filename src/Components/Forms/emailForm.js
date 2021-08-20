a1c3b9f645d980477bd1b66a549cadac7adf3c0c
rwellingtonr <wellington-158@hotmail.com>
2021-08-19 23:23:10 -0300

    Styled Login page

--- src/Components/Forms/emailForm.js
+++ src/Components/Forms/emailForm.js
@@ -1,12 +1,15 @@
 const emailField = ({ onEmailChange }) => {
   return (
     <div className="mt3">
-      <label className="db fw4 lh-copy f5" htmlFor="email-address">
+      <label
+        className="db pv1 near-white fw4 lh-copy f5"
+        htmlFor="email-address"
+      >
         Email address
       </label>
       <input
         onChange={onEmailChange}
-        className="pa2 input-reset ba b--white bg-transparent hover-bg-black hover-white w-80"
+        className="pa2 input-reset ba b--silver bg-transparent hover-bg-black hover-white w-80"
         type="email"
         required
         name="email-address"
--- src/Components/Forms/passwordForm.js
+++ src/Components/Forms/passwordForm.js
@@ -1,13 +1,13 @@
 const passwordForm = ({ onPasswordChange, handleKeyPress }) => {
   return (
     <div className="mv3">
-      <label className="db fw6 lh-copy f5" htmlFor="password">
+      <label className="db pv1 near-white fw6 lh-copy f5" htmlFor="password">
         Password
       </label>
       <input
         onKeyPress={handleKeyPress}
         onChange={onPasswordChange}
-        className="b pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-80"
+        className="b pa2 input-reset ba b--silver bg-transparent hover-bg-black hover-white w-80"
         type="password"
         name="password"
         id="password"
